a930cbc5ecca4962de883d0cd8f0649840fcd507
Juan José Abarca Garay <j.abarcag@outlook.com>
2025-06-23 22:03:13 +0000

    fix(*): modify selector

--- test_busqueda.py
+++ test_busqueda.py
@@ -1,9 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 import time
+import tempfile
 
-driver = webdriver.Chrome()
+# Configurar opciones de Chrome
+options = Options()
+options.add_argument(f"--user-data-dir={tempfile.mkdtemp()}")
+options.add_argument("--headless")  # Puedes quitar esta línea si quieres ver la ventana
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+# Iniciar navegador
+driver = webdriver.Chrome(options=options)
 driver.get("https://duckduckgo.com/")
 
 # Buscar campo de texto
@@ -15,10 +25,12 @@ buscador.send_keys(Keys.RETURN)
 time.sleep(2)
 
 # Validar que exista algún resultado
-resultados = driver.find_elements(By.CSS_SELECTOR,".result")
-
+#resultados = driver.find_elements(By.CSS_SELECTOR, ".result")
+resultados = driver.find_elements(By.CSS_SELECTOR, 'li[data-layout="organic"]')
 assert len(resultados) > 0, "No se encontraron resultados."
 
-print(" Prueba funcional completada con éxito")
+print(resultados)
+
+print("✅ Prueba funcional completada con éxito")
 
 driver.quit()
